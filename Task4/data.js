4c9ff5336aba9db56e8b53e7e4d76cb4d9351a07
Kiruthiga <107568764+Kiruthiga-M@users.noreply.github.com>
2024-02-02 21:44:16 +0530

    Add files via upload

---
+++ Task4/data.js
@@ -0,0 +1,38 @@
+let movies = [
+    {
+        name: 'Joe',
+        des: 'Joe, who is reeling from a heartbreak by his college sweetheart, reluctantly enters an arranged marriage. What surprises await in this new chapter for him?',
+        image: 'images/movie1.jpg',
+        desc:'Romance | Drama | First Love',
+        btn:'Watch Now'
+    },
+    {
+        name: 'Parking',
+        des: ' A clash over a shared space transforms two men from diverse backgrounds into aggressors. Will ego triumph, or can shared intentions lead to resolution?',
+        image: 'images/movie2.webp',
+        desc:'Drama | Suspense | Conspiracy',
+        btn:'Watch Now'
+    },
+    {
+        name: 'Neru',
+        des: 'Former lawyer Vijayamohan takes on the challenging case of a blind girl with unusual evidence. How will he navigate the legal odds and secure justice for her?',
+        image: 'images/movie3.webp',
+        desc:'Drama | Suspense | Gripping',
+        btn:'Watch Now'
+    },
+    {
+        name: 'Chithha',
+        des: '  Eswaran aka Chithhas world crumbles when his beloved niece goes missing. Will this emotional pursuit test the limits of love?',
+        image: 'images/movie4.webp',
+        desc:'Drama | Thriller | Revenge',
+        btn:'Watch Now'
+    },
+    {
+        name: 'Extra',
+        des: 'Abhinay, a striving junior artist, becomes CEO of his lovers company. Stardom beckons, but a mysterious encounter with Nero alters his path dramatically.',
+        image: 'images/movie5.webp',
+        desc:'Action | Comedy | Curious',
+        btn:'Watch Now'
+
+    }
+]
